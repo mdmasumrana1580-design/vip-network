9de0348dd004aed575f9d440442fcb466b333e6d
Md-MASUM RANA <mdmasumrana1580@gmail.com>
2026-09-08 21:29:44 +0600

    Add files via upload

---
+++ telegram-replace.js
@@ -0,0 +1,10 @@
+(function(){
+  var a=document.querySelector('.actions .messenger-btn');
+  if(!a)return;
+  a.className='telegram-float-btn';
+  a.href='https://t.me/+aUa0t1JubzFkMGRl';
+  a.target='_blank';
+  a.rel='noopener noreferrer';
+  a.setAttribute('aria-label','Telegram');
+  a.innerHTML='<img src="telegram-icon.svg" alt="Telegram">';
+})();
