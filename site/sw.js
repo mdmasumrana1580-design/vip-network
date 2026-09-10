2e8f231c47216ba394740220ac8994384b9cc217
Md-MASUM RANA <mdmasumrana1580@gmail.com>
2026-09-10 19:42:32 +0600

    Add files via upload

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vip-tv-pwa-v2';
+const CACHE = 'vip-tv-pwa-v3';
 const CORE = [
   '/',
   '/index.html',
@@ -11,7 +11,11 @@ const CORE = [
   '/vip-network-logo.png',
   '/vip-tv-logo-192.png',
   '/vip-tv-logo-512.png',
-  '/messenger-icon.webp'
+  '/messenger-icon.webp',
+  '/telegram-float.css',
+  '/telegram-3d.png',
+  '/vip-round.png',
+  '/welcome.mp4'
 ];
 
 self.addEventListener('install', event => {
